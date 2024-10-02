fa7a905f880a4a9ba16bc7fd53a5408c1e0adecd
Ermuhamet <ermyhamet2002@gmail.com>
2024-10-02 18:08:57 +0600

    Added factory for create shapes

---
+++ ShapeLibrary/ShapeFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace ShapeLibrary;
+
+/// <summary>
+/// Для реализации возможности вычисления площади без знания типа фигуры во время компиляции, можно использовать фабрику
+/// </summary>
+public static class ShapeFactory
+{
+    public static IShape CreateShape(string shapeType, params double[] parameters)
+    {
+        return shapeType.ToLower() switch
+        {
+            "circle" => new Circle(parameters[0]),
+            "triangle" => new Triangle(parameters[0], parameters[1], parameters[2]),
+            _ => throw new ArgumentException("Unknown shape type"),
+        };
+    }
+}
