3c4ede6adad212a30cf501e5733af8a39c4b8de3
Ermuhamet <ermyhamet2002@gmail.com>
2024-10-02 18:15:15 +0600

    Added unit test

---
+++ ShapeTests/ShapeTests.cs
@@ -0,0 +1,36 @@
+﻿using ShapeLibrary;
+using Xunit;
+using Assert = Xunit.Assert;
+
+
+namespace ShapeTests;
+
+public class ShapeTests
+{
+    [Fact]
+    public void CircleAreaTest()
+    {
+        var circle = new Circle(5);
+        Assert.Equal(Math.PI * 25, circle.GetArea(), 2);
+    }
+    [Fact]
+    public void TriangleAreaTest()
+    {
+        var triangle = new Triangle(3, 4, 5);
+        Assert.Equal(6, triangle.GetArea(), 2);
+    }
+
+    [Fact]
+    public void RightAngledTriangleTest()
+    {
+        var triangle = new Triangle(3, 4, 5);
+        Assert.True(triangle.IsRightAngled());
+    }
+
+    [Fact]
+    public void NonRightAngledTriangleTest()
+    {
+        var triangle = new Triangle(5, 6, 7);
+        Assert.False(triangle.IsRightAngled());
+    }
+}
